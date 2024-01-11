fa31565a006a94b8dea39d40beb32e67beb8b7ef
Avinash <javinash140@gmail.com>
2024-01-12 03:33:08 +0530

    added a new Feature

--- src/context/ProductContext.js
+++ src/context/ProductContext.js
@@ -27,17 +27,28 @@ export const ProductProvider = ({ children }) => {
    const existingItem = cart.find((item) => item.id === product.id);
 
     if (existingItem) {
+      const newQuantity = existingItem.quantity + 1;
+      if (newQuantity > product.quantity) {
      
+        alert(`Cannot add more than ${product.quantity} ${product.name}`);
+        return;
+      }
+
       setCart((prevCart) =>
         prevCart.map((item) =>
           item.id === product.id ? { ...item, quantity: item.quantity + 1 } : item
         )
       );
     } else {
-
+      if (1 > product.quantity) {
+     
+        alert(`Cannot add more than ${product.quantity} ${product.name}`);
+        return;
+      }
       setCart((prevCart) => [...prevCart, { ...product, quantity: 1 }]);
     }
   };
+
 
   const increaseQuantity = (productId) => {
     setCart((prevCart) =>
